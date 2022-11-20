05821b8ec67d40d139d13b1fa255d4bfbcce8d8c
myloserdova <112957753+myloserdova@users.noreply.github.com>
2022-11-20 10:26:35 +0000

    Deploying to gh-pages from @ myloserdova/parcel-bl-11@d2468b73c2eec6ea6b0b129d5ae0833622e912e8 🚀

---
+++ index.8cd4135c.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!0,prevArrow:'<button type="button" class="my-slick-prev">&xlarr;</button>',nextArrow:'<button type="button" class="my-slick-next">&xrarr;</button>',infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0});
+//# sourceMappingURL=index.8cd4135c.js.map
---
+++ index.e34796d1.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!0,prevArrow:'<button type="button" class="my-slick-prev">&xlarr;</button>',nextArrow:'<button type="button" class="my-slick-next">&xrarr;</button>',infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0});
+//# sourceMappingURL=index.e34796d1.js.map
